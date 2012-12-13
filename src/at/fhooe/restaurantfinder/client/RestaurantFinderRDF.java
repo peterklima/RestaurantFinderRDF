161fccd287d8e73c973b680cde3450a293c6d38b
Fabian Meisinger <fabian.meisinger@pro3games.com>
2012-12-13 14:21:22 +0100

    added jena libraries + example code

--- src/at/fhooe/restaurantfinder/client/RestaurantFinderRDF.java
+++ src/at/fhooe/restaurantfinder/client/RestaurantFinderRDF.java
@@ -1,6 +1,8 @@
 package at.fhooe.restaurantfinder.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -8,10 +10,20 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class RestaurantFinderRDF implements EntryPoint {
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
 		RootPanel.get().add(new HTML("Restaurant Finder loaded."));
+		String input = "foo";
+		greetingService.greetServer(input, new AsyncCallback<String>() {
+			public void onFailure(Throwable caught) {
+			}
+
+			@Override
+			public void onSuccess(String result) {
+			}
+		});
 	}
 }
--- src/at/fhooe/restaurantfinder/server/GreetingServiceImpl.java
+++ src/at/fhooe/restaurantfinder/server/GreetingServiceImpl.java
@@ -3,6 +3,13 @@ package at.fhooe.restaurantfinder.server;
 import at.fhooe.restaurantfinder.client.GreetingService;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
 /**
  * The server side implementation of the RPC service.
@@ -18,14 +25,31 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
 
+		try {
+			String queryStr = "select distinct ?Concept where {[] a ?Concept} LIMIT 10";
+			Query query = QueryFactory.create(queryStr);
+
+			// Remote execution.
+			QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+			// Set the DBpedia specific timeout.
+			((QueryEngineHTTP) qexec).addParam("timeout", "10000");
+
+			// Execute.
+			ResultSet rs = qexec.execSelect();
+			ResultSetFormatter.out(System.out, rs, query);
+			qexec.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>" + userAgent;
 	}
 
 	/**
-	 * Escape an html string. Escaping data received from the client helps to
-	 * prevent cross-site script vulnerabilities.
+	 * Escape an html string. Escaping data received from the client helps to prevent cross-site script vulnerabilities.
 	 * 
-	 * @param html the html string to escape
+	 * @param html
+	 *            the html string to escape
 	 * @return the escaped string
 	 */
 	private String escapeHtml(String html) {
