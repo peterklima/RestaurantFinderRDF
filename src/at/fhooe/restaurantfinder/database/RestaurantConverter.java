8aeb4ad6999cc18cbb5fa250ea55a02aafd9c8e1
Fabian Meisinger <fabian.meisinger@pro3games.com>
2013-01-09 21:50:49 +0100

    added support for additional properties

--- src/at/fhooe/restaurantfinder/database/RestaurantConverter.java
+++ src/at/fhooe/restaurantfinder/database/RestaurantConverter.java
@@ -1,12 +1,18 @@
 package at.fhooe.restaurantfinder.database;
 
+import java.util.List;
+
+import at.fhooe.restaurantfinder.shared.bo.BusinessHours;
+import at.fhooe.restaurantfinder.shared.bo.Category;
 import at.fhooe.restaurantfinder.shared.bo.Restaurant;
+import at.fhooe.restaurantfinder.shared.bo.Tag;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class RestaurantConverter extends BOConverter {
 	private static final String NAME_PROPERTY = "label";
 	private static final String DESCRIPTION_PROPERTY = "description";
+	private static final String HOURS_PROPERTY = "businessHours";
 
 	public static void addToModel(Restaurant restaurant, Model model) {
 		RestaurantConverter restaurantConverter = new RestaurantConverter(model);
@@ -21,7 +27,32 @@ public class RestaurantConverter extends BOConverter {
 		this.resource = model.createResource(restaurant.getId());
 		addNameIfNotNull(restaurant.getName());
 		addDescriptionIfNotNull(restaurant.getDescription());
+		addTagsIfNotNull(restaurant.getTags());
+		addCategoryIfNotNull(restaurant.getCategory());
+		addBusinessHoursIfNotNull(restaurant.getHours());
 		AddressConverter.addToModel(restaurant.getAddress(), model);
+	}
+
+	private void addBusinessHoursIfNotNull(List<BusinessHours> hours) {
+		if (hours != null) {
+			for (BusinessHours hour : hours) {
+				addLiteral(SDBWrapper.PREFIX_LOCAL, HOURS_PROPERTY, hour.toString());
+			}
+		}
+	}
+
+	private void addTagsIfNotNull(List<Tag> tags) {
+		if (tags != null) {
+			for (Tag tag : tags) {
+				addLiteral(SDBWrapper.PREFIX_LOCAL, "tag", tag.getName());
+			}
+		}
+	}
+
+	private void addCategoryIfNotNull(Category category) {
+		if (category != null) {
+			addLiteral(SDBWrapper.PREFIX_LOCAL, "category", category.getName());
+		}
 	}
 
 	private void addNameIfNotNull(String name) {
